781d6bd76d8ba604991face9ee67a0dc5bd66daf
Pritam <84267577+Pritam-Stats@users.noreply.github.com>
2025-01-08 20:48:41 +0530

    Add files via upload

---
+++ Day-6.py
@@ -0,0 +1,12 @@
+# code blocks, functions, while loop
+
+# FUNCTION
+
+def my_function():  # the parenthesis after the name differentiate a function from a variable
+    print("Hello")
+    print("Bye")
+
+my_function() # calling the function
+
+for i in range(1, 10, 2):
+    print(i)
